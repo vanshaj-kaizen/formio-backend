14f501ebbac7d56a58448be0b936ce7bf27959fd
Vanshaj <vanshajkaizen@gmail.com>
2025-09-19 12:01:10 +0530

    ssl added

--- db.js
+++ db.js
@@ -5,7 +5,10 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: true // or false if you don’t want to verify CA
+    }
 })
 
 module.exports = pool;
